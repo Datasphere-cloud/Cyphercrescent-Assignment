7cefca1a6c317b25a0bc5f1b9febd581098e7285
Datasphere-cloud <brighten0256@gmail.com>
2022-03-28 10:48:09 -0800

    I hope this works this time

---
+++ Conditional Statement/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter the values of a: ");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the values of b: ");
+
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the values of c: ");
+
+int c = int.Parse(Console.ReadLine());
+
+int D = b*b - (4 * a * c);
+
+Console.Write("Determinat: " + D);
+
+Console.WriteLine();
+
+if (D>0)
+{
+    Console.WriteLine("Hurray! Roots of equation are real");
+    if (D<0)
+    {
+        Console.WriteLine("Wow! Roots of equation are complex");
+        if (D==0)
+        {
+            Console.WriteLine("Roots are equal");
+        }
+    }
+}
+
+Console.Beep();
+
+
